7570e8189180dd4b4a9572cb831d802c1aab2e6a
Marek Olšák <olsak.marek@outlook.cz>
2023-11-26 16:55:42 +0100

    #7 - Replaced page directive for the route attribute.

---
+++ src/PocketStorage.Client/Routes.cs
@@ -0,0 +1,14 @@
+﻿namespace PocketStorage.Client;
+
+public static class Routes
+{
+    public const string Homepage = "/";
+    public const string Privacy = "/privacy";
+
+    public static class User
+    {
+        public const string Base = "/user";
+
+        public const string Claims = Base + "/claims";
+    }
+}
